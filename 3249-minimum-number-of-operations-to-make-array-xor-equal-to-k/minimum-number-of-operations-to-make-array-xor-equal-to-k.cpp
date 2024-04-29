68921b995b398bd67ef7e5567efd4d9e4281e907
Ansh1040 <121428304+Ansh1040@users.noreply.github.com>
2024-04-29 23:09:26 +0530

    Time: 108 ms (64.05%) | Memory: 91.2 MB (85.86%) - LeetSync

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        int ans = 0;
-        for (auto& x : nums) {
-            ans = ans ^ x;
+        // int ans = 0;
+        // for (auto& x : nums) {
+        //     ans = ans ^ x;
+        // }
+        // ans = ans ^ k;
+        // int res = 0;
+        // while (ans > 0) {
+        //     if (ans & 1)
+        //         res++;
+        //     ans = ans >> 1;
+        // }
+        // return res;
+
+        
+        int xors =k;
+        for(auto it :nums){
+            xors ^=it;
         }
-        ans = ans ^ k;
-        int res = 0;
-        while (ans > 0) {
-            if (ans & 1)
-                res++;
-            ans = ans >> 1;
-        }
-        return res;
+//         xors with k and count no of setbits as operations
+        // xors =xors^k;
+        return __builtin_popcount(xors);
     }
 };
