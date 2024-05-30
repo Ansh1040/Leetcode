5a261e139f76492c849344d0edc2b053e94cfe0b
Ansh1040 <121428304+Ansh1040@users.noreply.github.com>
2024-05-31 02:32:07 +0530

    Time: 5 ms (39.61%) | Memory: 9.3 MB (36.98%) - LeetSync

--- 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
+++ 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
@@ -1,26 +1,51 @@
 class Solution {
 public:
     int countTriplets(vector<int>& arr) {
-        int n = arr.size();
-        int pairs = 0;
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                int a = 0;
-                for(int k=i; k<j; k++){
-                    a ^= arr[k];
-                }
+        // int n = arr.size();
+        // int pairs = 0;
+        // for(int i=0; i<n; i++){
+        //     for(int j=i+1; j<n; j++){
+        //         int a = 0;
+        //         for(int k=i; k<j; k++){
+        //             a ^= arr[k];
+        //         }
                 
-                int b = 0;
+        //         int b = 0;
 
-                for(int k=j; k<n; k++){
-                    b ^= arr[k];
-                    if(a == b){
-                        pairs++;
-                    }
-                }
-            }
+        //         for(int k=j; k<n; k++){
+        //             b ^= arr[k];
+        //             if(a == b){
+        //                 pairs++;
+        //             }
+        //         }
+        //     }
             
+        // }
+        // return pairs;                                 brute force
+
+        vector<int> prefixXor(begin(arr), end(arr));
+
+        prefixXor.insert(prefixXor.begin(), 0); //initially the xor cumulative will be 0
+        int n = prefixXor.size();
+
+        for(int i = 1; i < n; i++) {
+            prefixXor[i] ^= prefixXor[i-1];
         }
-        return pairs;
+
+        int triplets = 0;
+
+        for(int i = 0; i < n; i++) {
+            for(int k = i+1; k < n; k++) {
+
+                if(prefixXor[k] == prefixXor[i]) {
+                    triplets += k-i-1;
+                }
+
+            }
+        }
+
+        return triplets;
+
+
     }
 };
