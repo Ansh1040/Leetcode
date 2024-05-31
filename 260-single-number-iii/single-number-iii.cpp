c00838cbe15d112b4c1b20241a8d88026235fd02
Ansh1040 <121428304+Ansh1040@users.noreply.github.com>
2024-06-01 02:17:39 +0530

    Time: 5 ms (79.17%) | Memory: 12.2 MB (87.19%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        int ans =0;
+        int a=0,b=0;
+
+        for(int i=0 ; i<nums.size() ; i++){
+            ans ^=nums[i];
+        }
+
+        int SetBit = ans & -(unsigned int)ans;   
+
+        for (int num : nums) {
+            if (num & SetBit) {
+                a ^= num;
+            } else {
+                b ^= num;
+            }
+        }
+        return {a,b};
+    }
+};
