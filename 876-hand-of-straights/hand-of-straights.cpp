2958aa2a81541c3fe746d2a569c7854361707301
Ansh1040 <121428304+Ansh1040@users.noreply.github.com>
2024-06-06 22:46:12 +0530

    Time: 31 ms (98.44%) | Memory: 23.1 MB (98.20%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+
+    bool find(vector<int>&hand , int groupSize , int i , int n){
+        int next = hand[i]+1;
+        hand[i] = -1;
+        int count = 1;
+        i += 1;
+
+        while( i < n && count < groupSize ){
+            if(hand[i] == next){
+                next  =hand[i]+1;
+                hand[i] = -1;
+                count++;
+            }
+            i++;
+        }
+        return count == groupSize ;
+    }
+
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        int n = hand.size();
+        if( n % groupSize != 0) return false;   
+
+        sort(hand.begin() , hand.end());
+
+        for(int i=0 ; i<n ; i++){
+            if(hand[i] >= 0 ){
+                if(!find(hand , groupSize , i , n )) return false;
+            }
+        }
+        return true;
+    }
+};
