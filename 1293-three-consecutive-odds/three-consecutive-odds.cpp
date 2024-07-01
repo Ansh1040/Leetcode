be19124d8e32bf40ea591c87bff256a90d06d0f0
Ansh1040 <121428304+Ansh1040@users.noreply.github.com>
2024-07-01 23:12:17 +0530

    Time: 4 ms (37.93%) | Memory: 10.6 MB (45.76%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.cpp
+++ 1293-three-consecutive-odds/three-consecutive-odds.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool threeConsecutiveOdds(vector<int>& arr) {
+        //         1.
+
         // for(int i=0 ; i<arr.size()-2 ; i++){
         //     if(arr[i]%2 != 0){
         //         if(arr[i+1]%2 !=0 ){
@@ -12,6 +14,10 @@ public:
         // }
         // return false;
 
+        
+
+        //          2.
+
         // for (int i = 0; i < arr.size() - 2; i++) { // Adjust the loop to avoid out-of-bounds
         //     if (arr[i] % 2 != 0 && arr[i+1] % 2 != 0 && arr[i+2] % 2 != 0) {
         //         return true; // If three consecutive odds are found, return true
@@ -20,20 +26,37 @@ public:
         // return false; // If no such triplet is found, return false
 
 
+        //          3.
 
-        int count = 0; // Initialize count to keep track of consecutive odd numbers
+        //     int count = 0; // Initialize count to keep track of consecutive odd numbers
+        //     for (int num : arr) { // Iterate through each element in the array
+        //         if (num % 2 != 0) { // Check if the current element is odd
+        //             count++; // Increment the count if it's odd
+        //             if (count == 3) { // If we have found three consecutive odds, return true
+        //                 return true;
+        //             }
+        //         } else { // If the element is even, reset the count to 0
+        //             count = 0;
+        //         }
+        //     }
+        //     return false; // If we finish the loop without finding three consecutive odds, return false
+        
 
-        for (int num : arr) { // Iterate through each element in the array
-            if (num % 2 != 0) { // Check if the current element is odd
-                count++; // Increment the count if it's odd
-                if (count == 3) { // If we have found three consecutive odds, return true
-                    return true;
+        //                4.
+
+    if(arr.size()==1||arr.size()==2)
+        return false;
+        for(int i=0;i<arr.size()-2;i++)
+        {
+            if(arr[i]%2!=0)
+            {
+                if(arr[i+1]%2 !=0 && arr[i+2]%2 !=0)
+                {
+                   return true;
                 }
-            } else { // If the element is even, reset the count to 0
-                count = 0;
             }
-        }
 
-        return false; // If we finish the loop without finding three consecutive odds, return false
+        }
+      return false;  
     }
 };
