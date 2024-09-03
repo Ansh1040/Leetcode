a9ec6fb2c3f83a26015b478be9c2d073af46d9df
Ansh1040 <121428304+Ansh1040@users.noreply.github.com>
2024-09-03 14:16:52 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (90.64%) - LeetSync

--- 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.cpp
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.cpp
@@ -1,21 +1,49 @@
 class Solution {
 public:
-    int getLucky(string s, int k) {
-        // Convert each character in the string to its corresponding numeric value
-        string number = "";
-        for (char x : s) {
-            number += to_string(x - 'a' + 1);
-        }
+    // int getLucky(string s, int k) {
+    //     // Convert each character in the string to its corresponding numeric value
+    //     string number = "";
+    //     for (char x : s) {
+    //         number += to_string(x - 'a' + 1);
+    //     }
         
-        // Perform the transformation `k` times
-        while (k > 0) {
-            int temp = 0;
-            for (char x : number) {
-                temp += x - '0';  // Sum the digits of the current number
-            }
-            number = to_string(temp);  // Convert the sum back to a string
-            k--;
+    //     // Perform the transformation `k` times
+    //     while (k > 0) {
+    //         int temp = 0;
+    //         for (char x : number) {
+    //             temp += x - '0';  // Sum the digits of the current number
+    //         }
+    //         number = to_string(temp);  // Convert the sum back to a string
+    //         k--;
+    //     }
+    //     return stoi(number);  // Return the final result as an integer
+    // }
+
+    int sumDigits(int num) {
+        int sum = 0;
+        while(num) {
+            sum += num%10;
+            num /= 10;
         }
-        return stoi(number);  // Return the final result as an integer
+        return sum;
     }
+    
+    int getLucky(string s, int k) {
+        int sum = 0 ; 
+        for(auto it: s){
+            int value = it - 'a' + 1;
+            if(value>=10){
+                sum+= value%10 + value/10; 
+            }else
+                sum = sum + value; 
+        }
+
+        k--;//since I did the sum once;
+        while(k-- && sum>=10){//furthur trasformations starts here
+            sum = sumDigits(sum);
+        } 
+        return sum;
+    }
+
+
 };
