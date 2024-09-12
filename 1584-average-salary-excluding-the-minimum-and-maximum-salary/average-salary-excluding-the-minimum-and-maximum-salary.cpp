74a081388ed9abf755f23f1a629cf190d9f3e6f3
Ansh1040 <121428304+Ansh1040@users.noreply.github.com>
2024-09-12 23:36:05 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (23.75%) - LeetSync

--- 1584-average-salary-excluding-the-minimum-and-maximum-salary/average-salary-excluding-the-minimum-and-maximum-salary.cpp
+++ 1584-average-salary-excluding-the-minimum-and-maximum-salary/average-salary-excluding-the-minimum-and-maximum-salary.cpp
@@ -1,15 +1,31 @@
 class Solution {
 public:
     double average(vector<int>& salary) {
+
+        //    O(n)
+
+        // double ans =0;
+        // int mn = INT_MAX , mx=INT_MIN;
+        // for(int i =0;i<salary.size();i++){
+        //     mn = min(mn , salary[i]);
+        //     mx = max(mx,salary[i]);
+        //     ans +=salary[i];
+        // }
+        // ans = ans - mn - mx;
+        // ans = ans/(salary.size()-2);
+        // return ans;
+
+
+
+        //    O(n(log(n)))
+
+        sort(salary.begin(), salary.end());
         double ans =0;
-        int mn = INT_MAX , mx=INT_MIN;
-        for(int i =0;i<salary.size();i++){
-            mn = min(mn , salary[i]);
-            mx = max(mx,salary[i]);
+        int n = salary.size();
+        for(int i=1; i<n-1;i++){
             ans +=salary[i];
         }
-        ans = ans - mn - mx;
-        ans = ans/(salary.size()-2);
+        ans = ans/(n-2);
         return ans;
     }
 };
