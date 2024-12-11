adc0ae26adc98fadf989bb4029f51080ea0c18c9
Ansh1040 <121428304+Ansh1040@users.noreply.github.com>
2024-12-12 01:45:30 +0530

    Time: 12 ms (93.70%) | Memory: 111.4 MB (46.40%) - LeetSync

--- 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     static int maximumBeauty(vector<int>& nums, int k) {
-        int freq[100001]={0};
-        int xMax=0, xMin=1e6;
-        for(int x: nums){
-            freq[x]++;
-            xMax=max(x, xMax);
-            xMin=min(x, xMin);
-        }
+        auto [xMin, xMax]=minmax_element(nums.begin(), nums.end());
+        const int a=*xMin, N=*xMax-a;
+        vector<int> freq(N+1, 0);
+        for(int x: nums)
+            freq[x-a]++;
+        
         int cnt=0, maxCnt=0;
-        for(int l=xMin, r=xMin; r<=xMax; r++){
+        for(int l=0, r=0; r<=N; r++){
             cnt+=freq[r];
-            while(r-l>2*k){
+            if (r-l>2*k){
                 cnt-=freq[l];
                 l++;
             }
