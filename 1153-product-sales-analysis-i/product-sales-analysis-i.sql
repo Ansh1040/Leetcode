9a2bf6e9ebd766bd1eafcb9918902d726dc04a53
Ansh1040 <121428304+Ansh1040@users.noreply.github.com>
2025-09-27 12:52:26 +0530

    Time: 1780 ms (5.50%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,5 +1,5 @@
 -- Write your PostgreSQL query statement below
-SELECT P.product_name,s.year,s.price 
+SELECT p.product_name,s.year,s.price 
 FROM Sales s JOIN Product p 
 ON p.product_id = s.product_id
 ORDER BY product_name
